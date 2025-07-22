87bb2f3a5ccbb36b22a1796c85cf97c29b53865a
Fire <80504137+gouthamsj2006@users.noreply.github.com>
2025-07-22 15:36:11 +0530

    Fix import paths in HireDeckPage.tsx

--- DDRL/project/src/pages/HireDeckPage.tsx
+++ DDRL/project/src/pages/HireDeckPage.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import HireDeckHeader from '../components/HireDeck/HireDeckHeader';
-import HireDeckDashboard from '../components/HireDeck/HireDeckDashboard';
-import HRSignup from '../components/HireDeck/HRSignup';
-import StudentSignup from '../components/HireDeck/StudentSignup';
+import HireDeckHeader from '../components/HireDeckHeader';
+import HireDeckDashboard from '../components/HireDeckDashboard';
+import HRSignup from '../components/HRSignup';
+import StudentSignup from '../components/StudentSignup';
 
 type ViewMode = 'dashboard' | 'hr-signup' | 'student-signup';
 
